fa7e2cc8df8bc8805cbbf7fb0a2d7a8df484a65f
MilanRyan <100083961+MilanRyan@users.noreply.github.com>
2023-03-20 16:56:27 +0800

    1 file modified

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ProductService {
   constructor() {}
-  Product() {
+  Product() { 
     return [
       {
         prod_id: 'P - 101',
